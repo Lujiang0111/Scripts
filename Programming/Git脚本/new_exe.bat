cd472dd9f64d112ee18131fd42bea9a50622167f
lujiang <lujiang0111@gmail.com>
2025-08-18 10:14:17 +0800

    更新脚本

--- Programming/Git脚本/new_exe.bat
+++ Programming/Git脚本/new_exe.bat
@@ -9,6 +9,13 @@ mkdir src
 cd src
 (
 echo #include ^<csignal^>
+echo #include ^<memory^>
+echo #include ^<string^>
+echo.
+echo struct Param
+echo {
+echo.
+echo };
 echo.
 echo bool app_running = true;
 echo.
@@ -18,9 +25,21 @@ echo     signal(SIGINT, SigIntHandler^);
 echo     app_running = false;
 echo }
 echo.
+echo std::shared_ptr^<Param^> ParseParam(int argc, char **argv^)
+echo {
+echo     std::shared_ptr^<Param^> param = std::make_shared^<Param^>(^);
+echo     return param;
+echo }
+echo.
 echo int main(int argc, char **argv^)
 echo {
 echo     signal(SIGINT, SigIntHandler^);
+echo.
+echo     std::shared_ptr^<Param^> param = ParseParam(argc, argv^);
+echo     if ^(!param^)
+echo     {
+echo         return 0;
+echo     }
 echo.
 echo     return 0;
 echo }
