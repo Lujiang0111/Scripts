ff1e20a463d4d2cda0fc6f70e6b0bdb0e157776e
Lujiang <lujiang0111@gmail.com>
2025-12-17 14:59:59 +0800

    添加颜色

--- Programming/Svn脚本/编译脚本/opensource/autobuild_automake.sh
+++ Programming/Svn脚本/编译脚本/opensource/autobuild_automake.sh
@@ -58,7 +58,7 @@ if [[ "${os_version}" == "centos7.1" ]]; then
 	gcc_major=${gcc_version%%.*}
 	if [ "${gcc_major}" -le 4 ]; then
 		source /opt/rh/devtoolset-8/enable
-		echo -e "switch to devtoolset-8"
+		echo -e "\033[34mswitch to devtoolset-8\033[0m"
 	fi
 fi
 
